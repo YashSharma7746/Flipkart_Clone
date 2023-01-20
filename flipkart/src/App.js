f943d5d2c4c54be1d2642cb10d2a7ecbd08be8c1
Sumit Pokhriyal <sumitstan1998@gmail.com>
2023-01-20 23:07:20 +0530

    added file

--- flipkart/src/App.js
+++ flipkart/src/App.js
@@ -1,23 +1,15 @@
 import './App.css';
-
-import { MobilesTablets } from './pages/Products/MobilesTablets/MobilesTablets';
 import AllRoutes from './Router/Router';
-
-
-
-import Home from './pages/Home/Home';
+import Navbar from './components/Navbar/Navbar';
 
 
 function App() {
  
   return (
 
-    <div className="App">
-   <AllRoutes/>
-
     <div className="App" style={{backgroundColor:"#F0F0F0"}}>
-      <Home />
-
+     <Navbar/>
+    <AllRoutes/>
     </div>
   );
 }
--- flipkart/src/index.js
+++ flipkart/src/index.js
@@ -6,6 +6,7 @@ import App from './App';
 import {BrowserRouter} from "react-router-dom"
 import { Provider } from 'react-redux';
 import {store} from "./redux/store"
+import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -17,19 +18,7 @@ root.render(
   </Provider>
   </ChakraProvider>
 
-import reportWebVitals from './reportWebVitals';
-import { ChakraProvider } from '@chakra-ui/react';
-import {BrowserRouter} from "react-router-dom";
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
-    </BrowserRouter>
-  </React.StrictMode>
-
 );
 
+reportWebVitals();
 
